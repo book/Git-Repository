691a314dbc1bbb479a58252dd772f15a4024f301
Philippe Bruhat (BooK) <book@cpan.org>
2010-05-13 12:27:41 +0200

    Git::Repository: clean up documentation template

--- lib/Git/Repository.pm
+++ lib/Git/Repository.pm
@@ -3,50 +3,37 @@ package Git::Repository;
 use warnings;
 use strict;
 
-=head1 NAME
-
-Git::Repository - The great new Git::Repository!
-
-=head1 VERSION
-
-Version 0.01
-
-=cut
-
 our $VERSION = '0.01';
 
+1;
+
+__END__
+
+=head1 NAME
+
+Git::Repository - Perl wrapper around git
 
 =head1 SYNOPSIS
 
-Quick summary of what the module does.
-
-Perhaps a little code snippet.
-
     use Git::Repository;
 
-    my $foo = Git::Repository->new();
-    ...
+    my $r = Git::Repository->new();
+    # new object from a working copy
+    my $r = Git::Repository->new( working_copy => $wc_path );
 
-=head1 EXPORT
+    # new object from a repository
+    my $r = Git::Repository->new( repository => $repo_path );
 
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
+    # new object from both
+    my $r = Git::Repository->new(
+        repository   => $repo_path,
+        working_copy => $wc_path
+    );
 
-=head1 FUNCTIONS
+    # run a command
+    my $output = $r->command( @cmd );
 
-=head2 function1
-
-=cut
-
-sub function1 {
-}
-
-=head2 function2
-
-=cut
-
-sub function2 {
-}
+=head1 DESCRIPTION
 
 =head1 AUTHOR
 
@@ -57,9 +44,6 @@ Philippe Bruhat (BooK), C<< <book at cpan.org> >>
 Please report any bugs or feature requests to C<bug-git-repository at rt.cpan.org>, or through
 the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Git-Repository>.  I will be notified, and then you'll
 automatically be notified of progress on your bug as I make changes.
-
-
-
 
 =head1 SUPPORT
 
@@ -94,14 +78,14 @@ L<http://search.cpan.org/dist/Git-Repository>
 =head1 ACKNOWLEDGEMENTS
 
 
-=head1 COPYRIGHT & LICENSE
+=head1 COPYRIGHT
 
 Copyright 2010 Philippe Bruhat (BooK), all rights reserved.
+
+=head1 LICENSE
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
 
-
 =cut
 
-1; # End of Git::Repository
