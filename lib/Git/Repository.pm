9cfbb4d8ad59493dd6cb45c88dd73021fc4829c9
Philippe Bruhat (BooK) <book@cpan.org>
2010-06-27 17:24:37 +0200

    Changes for version 1.04

--- lib/Git/Repository.pm
+++ lib/Git/Repository.pm
@@ -10,7 +10,7 @@ use Cwd qw( cwd abs_path );
 
 use Git::Repository::Command;
 
-our $VERSION = '1.03';
+our $VERSION = '1.04';
 
 # a few simple accessors
 for my $attr (qw( repo_path wc_path wc_subdir )) {
