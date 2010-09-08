f6511c45febe6b3d4e1663c949ee17c0aa3682a2
Philippe Bruhat (BooK) <book@cpan.org>
2010-09-08 22:53:45 +0200

    Git::Repository::Log: add some documentation

--- lib/Git/Repository/Log.pm
+++ lib/Git/Repository/Log.pm
@@ -65,3 +65,156 @@ sub new {
 
 __END__
 
+=head1 NAME
+
+Git::Repository::Log - Class representing git log data
+
+=head1 SYNOPSIS
+
+    # load the Log Mixin
+    use Git::Repository 'Log';
+
+    # get the log for last commit
+    my ($log) = Git::Repository->log( '-1' );
+
+    # get the author's email
+    print my $email = $log->author_email;
+
+=head1 DESCRIPTION
+
+C<Git::Repository::Log> is a class whose instances reprensent
+log items from a B<git log> stream.
+
+=head1 CONSTRUCTOR
+
+This method shouldn't be used directly. C<Git::Repository::Log::Iterator>
+should be the preferred way to create C<Git::Repository::Log> objects.
+
+=head2 new( @args )
+
+Create a new C<Git::Repository::Log> instance, using the list of key/values
+passed as parameters. The supported keys are (from the output of
+C<git log --pretty=raw>):
+
+=over 4
+
+=item commit
+
+The commit id (ignore the extra information added by I<--decorate>).
+
+=item tree
+
+The tree id.
+
+=item parent
+
+The parent list, separated by spaces.
+
+=item author
+
+The author information.
+
+=item committer
+
+The committer information.
+
+=item message
+
+The log message.
+
+=item extra
+
+Any extra text that might be added by extra options passed to B<git log>.
+
+=back
+
+=head1 ACCESSORS
+
+The following accessors methods are recognized. They all  return scalars,
+except for C<parent()>, which returns a list.
+
+=head2 Commit information
+
+=over 4
+
+=item commit
+
+=item tree
+
+=item parent
+
+=back
+
+=head2 Author and committer information
+
+=over 4
+
+=item author
+
+=item committer
+
+The original author/committer line
+
+=item author_name
+
+=item committer_name
+
+=item author_email
+
+=item committer_email
+
+=back
+
+=head2 Date information
+
+=over 4
+
+=item author_gmtime
+
+=item committer_gmtime
+
+=item author_localtime
+
+=item committer_localtime
+
+=item author_tz
+
+=item committer_tz
+
+=back
+
+=head2 Log information
+
+=over 4
+
+=item message
+
+=item subject
+
+=item body
+
+=back
+
+=head2 Extra information
+
+=over 4
+
+=item extra
+
+=back
+
+=head1 AUTHOR
+
+Philippe Bruhat (BooK), C<< <book at cpan.org> >>
+
+=head1 COPYRIGHT
+
+Copyright 2010 Philippe Bruhat (BooK), all rights reserved.
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
+
