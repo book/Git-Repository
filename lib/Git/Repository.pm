ec44cfe7630aaa58cc8035d43aa8d8d6a1b1a3f2
Philippe Bruhat (BooK) <book@cpan.org>
2010-10-02 17:54:46 +0200

    It seems that abs_path dies on Win32 with a path to a non-existent file
    The way the private _abs_path() is used in Git::Repository
    means failure will still happen as it should, when the path
    points to a non-existent file and abs_path() dies under Win32.

--- lib/Git/Repository.pm
+++ lib/Git/Repository.pm
@@ -26,11 +26,13 @@ for my $attr (qw( git_dir work_tree options )) {
 # helper function
 sub _abs_path {
     my ( $base, $path ) = @_;
-    return abs_path(
-        File::Spec->file_name_is_absolute($path)
-        ? $path
-        : File::Spec->catdir( $base, $path )
-    );
+    return eval {
+        abs_path(
+            File::Spec->file_name_is_absolute($path)
+            ? $path
+            : File::Spec->catdir( $base, $path )
+        );
+    };
 }
 
 #
